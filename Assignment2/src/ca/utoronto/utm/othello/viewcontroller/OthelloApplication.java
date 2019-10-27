6890b50cbd1f19a10d0dbb3d2b4d4373e90ffaef
Xu Wencai <wencai.xu@mail.utoronto.ca>
2019-10-27 14:12:30 -0400

    Add.gitignore file

--- Assignment2/src/ca/utoronto/utm/othello/viewcontroller/OthelloApplication.java
+++ Assignment2/src/ca/utoronto/utm/othello/viewcontroller/OthelloApplication.java
@@ -1,29 +1,39 @@
 package ca.utoronto.utm.othello.viewcontroller;
-import ca.utoronto.utm.othello.model.*;
+//import ca.utoronto.utm.othello.model.*;
+//
+//import javafx.application.Application;
+//import javafx.event.ActionEvent;
+//import javafx.geometry.Insets;
+//import javafx.scene.Scene;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.TextField;
+//import javafx.scene.layout.GridPane;
+//import javafx.scene.layout.HBox;
+//import javafx.scene.layout.VBox;
+//import javafx.stage.Stage;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-public class OthelloApplication extends Application {
+public class OthelloApplication  {
+		public static void main(String args[]) {
+			System.out.println(1);
+		}
+	}
 	// REMEMBER: To run this in the lab put 
 	// --module-path "/usr/share/openjfx/lib" --add-modules javafx.controls,javafx.fxml
 	// in the run configuration under VM arguments.
 	// You can import the JavaFX.prototype launch configuration and use it as well.
 	
-	@Override
-	public void start(Stage stage) throws Exception {
+//	public static void main(String[] args) {
+////		OthelloApplication view = new OthelloApplication();
+////		launch(args);
+//		System.out.print(1);
+//	}
+	
+	//@Override
+	//public void start(Stage stage) throws Exception {
 		// Create and hook up the Model, View and the controller
 		
 		// MODEL
-		Othello othello=new Othello();
+//		Othello othello=new Othello();
 		
 		// CONTROLLER
 		// CONTROLLER->MODEL hookup
@@ -32,19 +42,16 @@ public class OthelloApplication extends Application {
 		// VIEW->CONTROLLER hookup
 		// MODEL->VIEW hookup
 		
-		GridPane grid = new GridPane();
-		
-		// SCENE
-		Scene scene = new Scene(grid); 
-		stage.setTitle("Othello");
-		stage.setScene(scene);
-				
-		// LAUNCH THE GUI
-		stage.show();
-	}
+		//GridPane grid = new GridPane();
+//		HBox grid = new HBox(5);
+//		// SCENE
+//		Scene scene = new Scene(grid); 
+//		stage.setTitle("Othello");
+//		stage.setScene(scene);
+//				
+//		// LAUNCH THE GUI
+//		stage.show();
+//	}
 
-	public static void main(String[] args) {
-		OthelloApplication view = new OthelloApplication();
-		launch(args);
-	}
-}
+
+
---
+++ Assignment2/src/ca/utoronto/utm/othello/viewcontroller/fffa.java
@@ -0,0 +1,7 @@
+package ca.utoronto.utm.othello.viewcontroller;
+
+public class fffa {
+	public static void main(String args[]) {
+		System.out.println(1);
+	}
+}
