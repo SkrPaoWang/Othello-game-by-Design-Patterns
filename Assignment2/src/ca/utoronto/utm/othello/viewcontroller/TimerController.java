806a047cf9406edd8aea9209990c6d7f0e840175
Linfeng Zhou <zlf1204@gmail.com>
2019-11-14 21:08:05 -0500

    Merge conflict error fix

--- Assignment2/src/ca/utoronto/utm/othello/viewcontroller/TimerController.java
+++ Assignment2/src/ca/utoronto/utm/othello/viewcontroller/TimerController.java
@@ -21,23 +21,19 @@ public class TimerController implements EventHandler<ActionEvent> {
 	@Override
 	public void handle(ActionEvent event) {
 		String s = txt.getText();
-		try {
-			String num1 = s.split(":")[0];
-			String num2 = s.split(":")[1];
-			minutes = Integer.parseInt(num1);
-			seconds = Integer.parseInt(num2);
-			this.txt.setText(minutes + ":" + seconds);
-		} catch (Exception e) {
-			this.txt.setText("minutes:seconds");
-		}
+	    try {
+		    String num1 = s.split(":")[0];
+		    String num2 = s.split(":")[1];
+		    minutes = Integer.parseInt(num1);
+		    seconds = Integer.parseInt(num2);
+		    this.txt.setText(minutes + ":" + seconds);
+	    } catch (Exception e) {
+	    	this.txt.setText("minutes:seconds");
+	    }
 		if (minutes == 0 && seconds == 0) {
 			this.txt.setText("0:0");
 		}
-
-		if (seconds == 0) {
-			minutes--;
-			seconds = 60;
-		} else {
+		else {
 			if (seconds == 0) {
 				minutes--;
 				seconds = 60;
